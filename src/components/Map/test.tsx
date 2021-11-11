0c04456ac4fb337dcd730fdd9496bc2752dd302f
Crummorn <igorcaze@gmail.com>
2021-11-11 17:35:34 -0300

    Test: Map should render with markers in correct place.

--- src/components/Map/test.tsx
+++ src/components/Map/test.tsx
@@ -11,4 +11,32 @@ describe('<Map />', () => {
       })
     )
   })
+
+  it('should render with markers in correct place', () => {
+    const places = [
+      {
+        id: '1',
+        name: 'Petrolina',
+        slug: 'petrolina',
+        location: {
+          latitude: 0,
+          longitude: 0
+        }
+      },
+      {
+        id: '2',
+        name: 'Juazeiro',
+        slug: 'juazeiro',
+        location: {
+          latitude: 1,
+          longitude: 1
+        }
+      }
+    ]
+
+    render(<Map places={places} />)
+
+    expect(screen.getByTitle(/petrolina/i)).toBeInTheDocument()
+    expect(screen.getByTitle(/juazeiro/i)).toBeInTheDocument()
+  })
 })
