e7033fddece71068a3844a050838e1bda5f1cea9
Crummorn <igorcaze@gmail.com>
2022-01-12 14:57:48 -0300

    Feat: Abstract structure using HomeTemp.late

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,7 @@
-import { InfoOutline } from '@styled-icons/evaicons-outline'
-import LinkWrapper from 'components/LinkWrapper'
-import dynamic from 'next/dynamic'
+import HomeTemplate from './Home'
 
-const Map = dynamic(() => import('components/Map'), { ssr: false })
+import { MapProps } from 'components/Map'
 
-export default function Home() {
-  return (
-    <>
-      <LinkWrapper href="/about">
-        <InfoOutline size={32} aria-label="About" />
-      </LinkWrapper>
-
-      <Map />
-    </>
-  )
+export default function Home({ places }: MapProps) {
+  return <HomeTemplate places={places} />
 }
