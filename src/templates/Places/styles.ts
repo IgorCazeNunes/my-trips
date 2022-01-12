80e3d2ffeb2e0e66b41c9c686b85864e499c0e64
Crummorn <igorcaze@gmail.com>
2022-01-12 15:52:16 -0300

    Chore: Updated styles from places template.

---
+++ src/templates/Places/styles.ts
@@ -0,0 +1,54 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  padding: var(--large) var(--medium);
+`
+
+export const Container = styled.section`
+  margin: auto;
+
+  max-width: var(--container);
+`
+
+export const Heading = styled.h1`
+  margin-bottom: var(--medium);
+
+  font-size: var(--large);
+`
+
+export const Body = styled.div`
+  margin-bottom: var(--large);
+
+  p {
+    margin-bottom: var(--medium);
+  }
+`
+
+export const Gallery = styled.div`
+  display: grid;
+  grid-gap: var(--medium);
+
+  img {
+    background: #f6f7f8;
+    background-image: linear-gradient(
+      to right,
+      #f6f7f8 0%,
+      #edeef1 20%,
+      #f6f7f8 40%,
+      #f6f7f8 100%
+    );
+    background-size: 80rem 14rem;
+
+    animation: placeholderShimmer 1s linear infinite forwards;
+
+    @keyframes placeholderShimmer {
+      0% {
+        background-position: -40rem 0;
+      }
+
+      100% {
+        background-position: 40rem 0;
+      }
+    }
+  }
+`
