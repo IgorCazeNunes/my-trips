3fb1daa85a3dc9be8b6d511796b3ab3099663cb1
Crummorn <igorcaze@gmail.com>
2022-01-12 15:43:15 -0300

    Feat: Add navigation from map to place page.

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,3 +1,5 @@
+import { useRouter } from 'next/dist/client/router'
+
 import { MapContainer, TileLayer, Marker } from 'react-leaflet'
 
 type MarkerProps = {
@@ -14,29 +16,40 @@ export type MapProps = {
   places?: MarkerProps[]
 }
 
-const Map = ({ places }: MapProps) => (
-  <MapContainer
-    center={[0, 0]}
-    zoom={3}
-    style={{ height: '100%', width: '100%' }}
-  >
-    <TileLayer
-      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
+const Map = ({ places }: MapProps) => {
+  const router = useRouter()
 
-    {places?.map(({ id, name, location }: MarkerProps) => {
-      const { latitude, longitude } = location
+  const handleNavigate = (slug: string) => {
+    router.push(`/place/${slug}`)
+  }
 
-      return (
-        <Marker
-          key={`marker-place-${id}`}
-          position={[latitude, longitude]}
-          title={name}
-        />
-      )
-    })}
-  </MapContainer>
-)
+  return (
+    <MapContainer
+      center={[0, 0]}
+      zoom={3}
+      style={{ height: '100%', width: '100%' }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+
+      {places?.map(({ id, slug, name, location }: MarkerProps) => {
+        const { latitude, longitude } = location
+
+        return (
+          <Marker
+            key={`marker-place-${id}`}
+            position={[latitude, longitude]}
+            title={name}
+            eventHandlers={{
+              click: () => handleNavigate(slug)
+            }}
+          />
+        )
+      })}
+    </MapContainer>
+  )
+}
 
 export default Map
