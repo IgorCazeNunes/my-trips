c4cf4411c6831d80a093d1e70df24a377d8c395e
Crummorn <igorcaze@gmail.com>
2022-01-12 16:18:58 -0300

    Feat: Add loader with NProgress.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from 'next/app'
 import Head from 'next/head'
+import NextNProgress from 'nextjs-progressbar'
 
 import GlobalStyles from 'styles/global'
 
@@ -26,6 +27,13 @@ function App({ Component, pageProps }: AppProps) {
       </Head>
 
       <GlobalStyles />
+
+      <NextNProgress
+        color="#f231a5"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+      />
 
       <Component {...pageProps} />
     </>
